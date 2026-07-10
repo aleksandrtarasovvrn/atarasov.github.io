7fb723fbfc73df32cea11512913a1ebc4aa6f9fa
aleksandrtarasovvrn <aleksandrtarasov.vrn@gmail.com>
2026-07-10 22:35:26 +0300

    go

---
+++ script.js
@@ -0,0 +1,8 @@
+const themeToggle = document.getElementById('themeToggle');
+
+if (themeToggle) {
+  themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    themeToggle.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
+  });
+}
